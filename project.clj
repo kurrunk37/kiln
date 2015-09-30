f115e3a2b22e50c2ef5fa0d773caa84d96ceee95
huzhengquan <froons@gmail.com>
2015-09-30 09:12:28 +0800

    first

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject kiln "0.1.0-SNAPSHOT"
+  :description "静态网站生成器"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [stencil "0.5.0"]
+                 [markdown-clj "0.9.74"]]
+  :main ^:skip-aot kiln.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/kiln/core_test.clj
@@ -0,0 +1,7 @@
+(ns kiln.core-test
+  (:require [clojure.test :refer :all]
+            [kiln.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
